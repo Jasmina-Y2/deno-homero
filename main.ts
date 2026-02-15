67048f0a1beb51a9a59bbd6c01966771b3337ae4
Grace Llacua <llacuagrayce@gmail.com>
2026-02-15 14:05:01 -0500

    agregado cors

--- main.ts
+++ main.ts
@@ -7,15 +7,25 @@ import awsRoutes from "./src/routes/aws.ts";
 const app = new Application();
 const PORT = 8000;
 
-app.use(oakCors());
+app.use(oakCors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}));
 
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.headers.get("X-Response-Time");
+    console.log(`${ctx.request.method} ${ctx.request.url} - ${ctx.response.status}`);
+});
 
+// Rutas
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.use(awsRoutes.routes());
 app.use(awsRoutes.allowedMethods());
 
-
-console.log(`🦕 Servidor Deno escuchando en http://localhost:${PORT}`);
+console.log(`🦕 Servidor Deno escuchando en puerto ${PORT}`);
 await app.listen({ port: PORT });
